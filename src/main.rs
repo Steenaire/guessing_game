0af0f4ab52e0f26ef0aafbfab975472332a36ec3
Steen Clarke <cmclarkey@gmail.com>
2016-09-30 14:29:40 -0700

    Make guessing game, move assignment out of loop so number doesn't change every time you guess

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+extern crate rand;
+
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+    println!("Guess the number!");
+    println!("Enter your guess:");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    
+    loop {
+        let mut guess = String::new();
+
+        println!("The secret number is: {}", secret_number);
+
+        io::stdin().read_line(&mut guess)
+        .expect("Failed to read input");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Higher!"),
+            Ordering::Greater => println!("Lower!"),
+            Ordering::Equal => {
+                println!("You Win!");
+                break;    
+            }
+        }
+    }
+}
